59264e0c3e01a46cc1c2fea3d32d7cea354733f8
juhishw <juhi.shaw@quovantis.com>
2021-03-20 13:54:42 +0530

    constatnt file

--- src/app/core/constants/route.constants.ts
+++ src/app/core/constants/route.constants.ts
@@ -1,6 +1,3 @@
 export class RouteConstants {
     public static readonly LOGIN = 'login';
-    public static readonly DASHBOARD = 'dashboard';
-    public static readonly FORGOT_PASSWORD = 'forgot_password';
-    public static readonly RESET_PASSWORD = 'reset_password';
 }
